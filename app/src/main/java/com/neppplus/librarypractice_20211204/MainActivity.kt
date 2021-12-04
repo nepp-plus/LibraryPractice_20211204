41689275c576a043ef321815af0658ffcee7b3a4
nepp-plus <nepp.plus@gmail.com>
2021-12-04 16:21:18 +0900

    권한 확인 라이브러리 -TedPermission 사용 체험

--- app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.neppplus.librarypractice_20211204
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,12 +24,34 @@ class MainActivity : AppCompatActivity() {
 
 //            전화 연결 해도 되는지? 권한 확인 -> OK일때만 전화연결.
 
+//            권한 여부에 따른 행동 방침 변수
 
-//            010-5555-6666 에 실제 전화 연결
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
 
-            val myUri = Uri.parse("tel:010-5555-6666")
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+//                    권한 승인 됬을때 할 행동
+
+                    //            010-5555-6666 에 실제 전화 연결
+
+                    val myUri = Uri.parse("tel:010-5555-6666")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@MainActivity, "권한이 없어 전화 연결 불가합니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            }
+
+//            실제 권한 확인 요청
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions( Manifest.permission.CALL_PHONE )
+                .check()
+
 
         }
 
