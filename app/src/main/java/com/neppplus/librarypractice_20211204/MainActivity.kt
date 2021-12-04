e80e31e2b602fa0fadcd8dc351568c16cc665e24
nepp-plus <nepp.plus@gmail.com>
2021-12-04 15:16:53 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.neppplus.librarypractice_20211204
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+
+}
