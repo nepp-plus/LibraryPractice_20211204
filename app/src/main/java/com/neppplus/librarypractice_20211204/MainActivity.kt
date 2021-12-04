c04aecb97f9d704a4bb292fca3722b6e3f516e7e
nepp-plus <nepp.plus@gmail.com>
2021-12-04 15:44:04 +0900

    사진 클릭시 크게 보기 화면 진입  - 사진보기 화면 테마 - noactionbar 변경

--- app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.librarypractice_20211204
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+
+        imgProfile.setOnClickListener {
+
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
     }
 
